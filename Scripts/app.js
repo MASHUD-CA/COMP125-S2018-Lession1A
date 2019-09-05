0ebb42240050098620878612d05614310d48ad4f
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-09-05 02:16:00 -0400

    added IIFE to app.js

---
+++ Scripts/app.js
@@ -0,0 +1,13 @@
+/* Custom JS goes here */
+// This is a closure; IIFE = Immediately Invoked Function Expression
+//Anonymous self executing function
+(function(){
+
+    function Start(){
+        console.log("App Started!");
+        console.log("--------------------------------");
+        console.log("Hewllo, World!");
+    }
+    Start();
+
+})();
