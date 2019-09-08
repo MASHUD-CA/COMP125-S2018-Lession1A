e7177b0c2b72f0fd6270be3fc433a3f30f5149bf
Mashud Ahmed <mashudahmed1980@gmail.com>
2019-09-07 23:14:28 -0400

    added about page and simple title detection

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,11 +2,14 @@
 // This is a closure; IIFE = Immediately Invoked Function Expression
 //Anonymous self executing function
 (function(){
-
+    
     function Start(){
+        //local variable
+        var title = document.title;
+
         console.log("App Started!");
         console.log("--------------------------------");
-        console.log("Hewllo, World!");
+        console.log("Title: "+ title);
     }
     Start();
 
